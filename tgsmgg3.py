6a1f26f055fe49b5a212a4540c4ee336ce4b88e9
Audiansyah <msaudiansyah@gmail.com>
2023-03-10 10:40:38 +0700

    commit v1.2

--- tgsmgg3.py
+++ tgsmgg3.py
@@ -21,17 +21,15 @@ irene_adler = {
 
 orang = [arsene_lupin, sherlock_holmes, irene_adler]
 
-# membuat dictionary kosong
 orang = []
 
-# menerima input dari user dan menambahkan ke dalam dictionary
 for i in range(3):
     nama = input(f"Masukkan nama orang ke-{i+1}: ")
     nik = int(input(f"Masukkan NIK orang ke-{i+1}: "))
     jenis_kelamin = input(f"Masukkan jenis kelamin orang ke-{i+1} (male/female): ")
     tanggal_lahir = input(f"Masukkan tanggal lahir orang ke-{i+1} (format: YYYY-MM-DD): ")
     
-    # membuat dictionary untuk setiap orang
+
     data_orang = {
         'nama': nama,
         'nik': nik,
@@ -39,8 +37,26 @@ for i in range(3):
         'tanggal_lahir': tanggal_lahir
     }
     
-    # menambahkan dictionary orang ke dalam list orang
     orang.append(data_orang)
+
+    orang = [
+    {'nama': 'Arsene Lupin', 'nik': 1234, 'jenis_kelamin': 'male', 'tanggal_lahir': '1902-03-23'},
+    {'nama': 'Sherlock Holmes', 'nik': 4321, 'jenis_kelamin': 'male', 'tanggal_lahir': '1876-08-16'},
+    {'nama': 'Irene Adler', 'nik': 6789, 'jenis_kelamin': 'female', 'tanggal_lahir': '1884-10-07'}
+]
+
+nama_dicari = input("Masukkan nama yang ingin dicari: ")
+
+for data in orang:
+    if data['nama'].lower() == nama_dicari.lower():
+        print(f"Data ditemukan:")
+        print(f"Nama: {data['nama']}")
+        print(f"NIK: {data['nik']}")
+        print(f"Jenis kelamin: {data['jenis_kelamin']}")
+        print(f"Tanggal lahir: {data['tanggal_lahir']}")
+        break
+else:
+    print("Data tidak ada")
 
 
 print(orang)
